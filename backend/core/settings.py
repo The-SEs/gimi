64608c639a002825772b3c6ef07a5dd78913dd74
HoogaBoga <matty.lim718@gmail.com>
2026-03-12 09:25:39 +0800

    changed default user table

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'
-    'accounts'
+    'rest_framework',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -87,7 +87,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = 'accounts.CustomUser'
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
