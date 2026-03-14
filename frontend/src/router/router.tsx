14b009927ff7d4babd48641daca20db599b898dd
toot-a-loot <mjartgithub@gmail.com>
2026-03-14 13:54:38 +0800

    Added Initial Folder Structure

---
+++ frontend/src/router/router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import pages here
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        // TODO : Add routes here // Example route code para home or default nga
+        page // <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
