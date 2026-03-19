d42ad7b7bb8995d2f6c636b13adca18d605e6252
MerakiiZ <christlezoe4@gmail.com>
2026-03-19 08:56:53 +0800

    Added music widget ui

--- frontend/src/pages/dashboard/dashboard.tsx
+++ frontend/src/pages/dashboard/dashboard.tsx
@@ -1,10 +1,19 @@
 import PictureWidget from "../../components/widget/picturewWidget";
 const sampleimage = "https://c.files.bbci.co.uk/18696/production/_132509999_gettyimages-827388844.jpg";
+import MusicPlayer from "../../components/widget/musicWidget";
+const song = "https://music.youtube.com/watch?v=3IUqoyTxEU8&si=zmV_TcSm-CrrOaTl"
+const cover ="https://c.files.bbci.co.uk/18696/production/_132509999_gettyimages-827388844.jpg";
 
 export default function Dashboard() {
   return(
-    <div>
+    <div className="flex gap-12">
       <PictureWidget image={ sampleimage } title="bleh" />
+      <MusicPlayer
+        title="Ocean Eyes"
+        artist="Billie Eilish"
+        src={song}
+        cover={cover}
+        />
     </div>
   )
 }
