7a5b0db9fcd75a65b5506cc0d8b9f82d1b5b3401
jamien <jamiencharisse@gmail.com>
2026-03-24 14:04:39 +0800

    style: add media queries style: change heart icon color

--- frontend/src/components/welcome-widget/WelcomeWidget.tsx
+++ frontend/src/components/welcome-widget/WelcomeWidget.tsx
@@ -1,5 +1,5 @@
 import welcomeIcon from "../../assets/welcome-icon.svg";
-// import mdWelcomeIcon from "../../assets/md-welcome-icon.svg";
+import lgWelcomeIcon from "../../assets/lg-welcome-icon.svg";
 import { useEffect, useState } from "react";
 
 
@@ -12,11 +12,11 @@ export default function WelcomeWidget () {
         day: 'numeric',
     });
 
-    const [isBiggerScreen, setIsBiggerScreen] = useState(window.innerWidth >= 768);
+    const [isBiggerScreen, setIsBiggerScreen] = useState(window.innerWidth >= 1024);
 
     useEffect(() => {
     const handleResize = () => {
-        setIsBiggerScreen(window.innerWidth >= 768);
+        setIsBiggerScreen(window.innerWidth >= 1024);
     };
 
     window.addEventListener('resize', handleResize);
@@ -25,11 +25,11 @@ export default function WelcomeWidget () {
     }, []);
 
     return (
-        <div className="relative overflow-hidden bg-white p-6 pb-20 rounded-xl w-[90%] mx-auto md:w-1/2 md:mx-0 md:text-left md:px-10 md:py-20">
+        <div className="relative overflow-hidden bg-white p-6 pb-20 mb-10 mt-5 rounded-xl w-[90%] mx-auto lg:w-[55%] lg:pb-35 lg:rounded-3xl xl:w-[65%] xl:pb-35">
             <div className="flex flex-col gap-1">
-                <p className="font-varela text-[#1E40AF] md:text-[18px] md:text-[#0C1326]">{formattedDate}</p>
-                <p className="font-varela font-bold text-2xl text-[#0F172A] md:text-[48px] md:text-[#1E3A8A] leading-tight">Hi, Princess!</p>
-                <p className="font-varela text-sm text-[#64748B] md:text-[18px] md:text-[#3B82F6] max-w-md">
+                <p className="font-varela text-[#1E40AF] lg:text-[18px] lg:text-[#0C1326]">{formattedDate}</p>
+                <p className="font-varela font-bold text-2xl text-[#0F172A] lg:text-[48px] lg:text-[#1E3A8A] leading-tight">Hi, Princess!</p>
+                <p className="font-varela text-sm text-[#64748B] lg:text-[18px] lg:text-[#3B82F6] max-w-md">
                     <span className="md:hidden">Ready for a cozy, productive day?</span>
                     <span className="hidden md:inline">
                         Your GIMI space is ready for some creative thoughts. Let's make today beautiful!
@@ -37,8 +37,8 @@ export default function WelcomeWidget () {
                 </p>
             </div>
 
-            {/* <img src={isBiggerScreen ? mdWelcomeIcon : welcomeIcon} alt=""className={`absolute w-16 h-16 object-contain
-            ${isBiggerScreen ? 'top-6 right-6 w-24 h-24' : 'bottom-2 right-2 w-16 h-16'}`}/> */}
+            <img src={isBiggerScreen ? lgWelcomeIcon : welcomeIcon} alt=""className={`absolute w-16 h-16 object-contain
+            ${isBiggerScreen ? 'top-10 right-10 w-24 h-24' : 'bottom-2 right-2 w-16 h-16'}`}/>
         </div>
 
     );
