082404e8c41b6baa4868469b25799dbbaac58b6f
DrineDev <ejcotejar@gmail.com>
2026-04-09 23:02:16 +0800

    fix: conflict with django admin

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("django-admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("api/auth/", include("dj_rest_auth.urls")),
     path("api/auth/registration/", include("dj_rest_auth.registration.urls")),
@@ -16,4 +16,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
